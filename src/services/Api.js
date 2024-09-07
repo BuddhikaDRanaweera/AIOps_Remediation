ad8c3910f572b8340234158ce6fff6aed82f807b
Buddhika <buddhikadilupulr@gmail.com>
2024-09-08 00:54:59 +0530

    update

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const apiUrl = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: apiUrl,
 });
 
 export default api;
