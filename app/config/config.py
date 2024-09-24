9f177e1862ddc8e5b5dcc7db2530bf8640237f78
Buddhika <buddhikadilupulr@gmail.com>
2024-09-24 10:06:14 +0530

    db config update

--- app/config/config.py
+++ app/config/config.py
@@ -3,9 +3,9 @@ import os
 class Config:
     # MySQL database configuration
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'demoadmin'
+    MYSQL_PASSWORD = "demoadmin"
     MYSQL_DB = 'aiops'
-    MYSQL_HOST = '13.213.71.181'
+    MYSQL_HOST = '13.229.202.159'
     MYSQL_PORT = 3306
     SQLALCHEMY_DATABASE_URI = f"mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
