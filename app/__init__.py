c8def613da44289f4052a24be6523f39755d6b8b
Buddhika <buddhikadilupulr@gmail.com>
2024-10-15 09:24:06 +0530

    email function added

--- app/__init__.py
+++ app/__init__.py
@@ -29,7 +29,7 @@ def create_app():
 
     # # Load configuration
     # app.config.from_object(Config(mysql_user, mysql_password, mysql_host, mysql_port, mysql_db))
-    app.config.from_object(Config('root', '1Str0ng!P@ss', 'localhost', '3306', 'aiops'))
+    app.config.from_object(Config('root', "1Str0ng!P@ss", 'localhost', '3306', 'aiops'))
 
     # Initialize extensions
     db.init_app(app)
